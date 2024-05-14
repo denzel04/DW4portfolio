569172a7d58e4e3592951bbb5a1ed33d5a9a5ce7
denzel04 <139096796+denzel04@users.noreply.github.com>
2024-05-13 22:45:52 -0600

    Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return   (
         <div className= 'hero'>
           <img src={profile_img} alt="" />
-          <h1><span>I'm Denzel Whittick,</span> fronted developer based in USA, Florida.</h1>
+          <h1><span>I'm Denzel Whittick,</span> front end developer based in USA, Florida.</h1>
           <p>I am a fronted developer from Florida with 2 years of experience working with HTML, CSS, React, JavaScript, and others.</p>
           <div className="hero-action">
             <div className="hero-connect">Connect</div>
